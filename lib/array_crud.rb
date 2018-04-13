58cd078510e87eaf9d88ac28b1c8cbd378d91101
Drin K <drinkaziu@gmail.com>
2018-04-13 16:55:58 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  dogs = ["Nacho", "Cooper"]
+  dogs = ["Nacho", "Cooper", "Tucker", "Chief"]
 end
 
 def add_element_to_end_of_array(array, element)
